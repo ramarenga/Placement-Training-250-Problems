4ba854074b9a0c3a5e01c39a95637776abe9d487
ramarenga <ramarenga97@gmail.com>
2026-05-28 12:56:57 +0530

    Time: 16 ms (34.63%), Space: 12.3 MB (70.85%) - LeetHub

---
+++ 0010-regular-expression-matching/0010-regular-expression-matching.py
@@ -0,0 +1,30 @@
+class Solution:
+    def isMatch(self, s, p):
+        m, n = len(s), len(p)
+
+        # DP table
+        dp = [[False] * (n + 1) for _ in range(m + 1)]
+        dp[0][0] = True
+
+        # Handle patterns like a*, a*b*, a*b*c*
+        for j in range(2, n + 1):
+            if p[j - 1] == '*':
+                dp[0][j] = dp[0][j - 2]
+
+        for i in range(1, m + 1):
+            for j in range(1, n + 1):
+
+                # Normal character or '.'
+                if p[j - 1] == s[i - 1] or p[j - 1] == '.':
+                    dp[i][j] = dp[i - 1][j - 1]
+
+                # Handle '*'
+                elif p[j - 1] == '*':
+                    # Zero occurrence
+                    dp[i][j] = dp[i][j - 2]
+
+                    # One or more occurrence
+                    if p[j - 2] == s[i - 1] or p[j - 2] == '.':
+                        dp[i][j] = dp[i][j] or dp[i - 1][j]
+
+        return dp[m][n]
