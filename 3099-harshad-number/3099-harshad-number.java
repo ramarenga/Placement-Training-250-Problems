94e63fe0c2f3faf693de2483d5a7755ffe0c187d
ramarenga <ramarenga97@gmail.com>
2026-06-17 14:55:09 +0530

    Time: 0 ms (100%), Space: 41.9 MB (79.21%) - LeetHub

---
+++ 3099-harshad-number/3099-harshad-number.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int sumOfTheDigitsOfHarshadNumber(int x) {
+
+        int sum = 0;
+        int num = x;
+
+        while (num > 0) {
+            sum = sum + (num % 10);
+            num = num / 10;
+        }
+
+        if (x % sum == 0) {
+            return sum;
+        }
+
+        return -1;
+    }
+}
